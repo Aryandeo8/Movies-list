bbd545ff09823d4d2606a967f7f6aa4a2f5ffabb
Aryandeo8 <aryandeo@gmail.com>
2025-02-09 01:23:14 +0530

    Added support to hide api keys

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,16 @@
-const API_KEY="4637acd1751c05039931fd38896de938"
-const BASE_URL="https://api.themoviedb.org/3"
-export const getPopularMovies = async()=>{
+import { REACT_APP_API_KEY, REACT_APP_BASE_URL } from "../../creds";
+
+const API_KEY = REACT_APP_API_KEY;
+const BASE_URL = REACT_APP_BASE_URL;
+
+export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-    const data= await response.json()
-    return data.results
+    const data = await response.json();
+    return data.results;
 };
-export const searchMovies = async(query)=>{
+
+export const searchMovies = async (query) => {
     const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
-    const data= await response.json()
-    return data.results
+    const data = await response.json();
+    return data.results;
 };
